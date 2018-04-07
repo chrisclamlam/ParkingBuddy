60c9f18cdf9056497faf9b27317ee33efde2776b
Christopher Lam <lam131@usc.edu>
2018-04-07 15:33:42 -0700

    login page finished and partial mapview

--- Frontend/Components/MapScreen.js
+++ Frontend/Components/MapScreen.js
@@ -6,15 +6,28 @@ export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <MapView
-        style={{ flex: 1 }}
-        initialRegion={{
-          latitude: 37.78825,
-          longitude: -122.4324,
-          latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421,
-        }}
-      />
+        <View style={{justifyContent:'center', alignItems:'center'}}>
+          <Text style={{ marginTop: '10%', fontSize:17, fontWeight: 'bold' }}> PARKING BUDDY </Text>
+        </View>
+        <TextInput
+          style={{
+            height: 40, borderColor: 'gray', borderWidth: 1, borderRadius: 10, padding: 10, width: '100%',
+            marginTop: 10, justifyContent: 'center', alignContent: 'center', overflow: 'hidden'
+          }}
+          onChangeText={(text) => this.setState({ inputUsername: text })}
+          placeholder='Search'
+        />
+        <View style={{ flex: 1 }}>
+          <MapView
+            style={{ flex: 1 }}
+            initialRegion={{
+              latitude: 37.78825,
+              longitude: -122.4324,
+              latitudeDelta: 0.0922,
+              longitudeDelta: 0.0421,
+            }}
+          />
+        </View>
       </View >
     );
   }
@@ -24,7 +37,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    // alignItems: 'center',
-    // justifyContent: 'center',
   },
 });
