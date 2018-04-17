cb2156267f268e9b2e9e23475e534fb2c77d621e
Dennis Naroditskiy <narodits@usc.edu>
2018-04-16 20:26:06 -0700

    Global Variables

--- Frontend/App.js
+++ Frontend/App.js
@@ -2,6 +2,12 @@ import React from 'react';
 import { StyleSheet, Text, View, TextInput } from 'react-native';
 import StackScreen from './Components/StackScreen.js'
 
+// Global variable for IP -> to use when fetching data from server
+global.serverIP = "http://10.123.112.238:8080/ParkingBuddy/";
+global.authKey = ""; // To hold auth key
+global.loggedIn = false; // To check if user is logged in
+
+
 export default class App extends React.Component {
   render() {
     return (
